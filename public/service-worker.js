05256b10347dac5802228c57e5d374f123ed014e
Surya Wiguna <suryawigunaa@gmail.com>
2019-12-10 09:20:09 +0800

    hapus event activate dan tambah event push di service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,17 +40,23 @@ workbox.routing.registerRoute(
     })
   );
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener('push', function(event) {
+    var body;
+    if (event.data) {
+      body = event.data.text();
+    } else {
+      body = 'Push message no payload';
+    }
+    var options = {
+      body: body,
+      icon: 'img/logo-sm.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      }
+    };
+    event.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    );
+  });
